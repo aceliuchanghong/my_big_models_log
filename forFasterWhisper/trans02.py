f647b631e15534d7ea56431e5cde72c1f10d88b6
lawrence <aceliuchanghong@gmail.com>
2024-01-06 16:07:40 +0800

    新增srt生成

--- forFasterWhisper/trans02.py
+++ forFasterWhisper/trans02.py
@@ -1,14 +1,17 @@
 # 我的本机上面跑的
 from faster_whisper import WhisperModel
+import pysubs2
 
 files1 = "../testfile/2.wav"
 files2 = "../testfile/WeChat_20231007161725.mp3"
+srt_file_name = '../testfile/out/test'
 # model_size = "large-v3"
 path = r'C:\Users\lawrence\Documents\large_v3'
 # Run on GPU with FP16
 # model = WhisperModel(model_size_or_path=path, device="cuda", compute_type="float16", local_files_only=True)
 
 # or run on GPU with INT8
+# 启动大概20s,跑files2大概80s
 model = WhisperModel(model_size_or_path=path, device="cuda", compute_type="int8_float16", local_files_only=True)
 # or run on CPU with INT8
 # model = WhisperModel(model_size_or_path=path, device="cpu", compute_type="int8", local_files_only=True)
@@ -18,5 +21,14 @@ segments, info = model.transcribe(audio=files2, language='zh', vad_filter=True,
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
+results = []
 for segment in segments:
     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+    segment_dict = {'start': segment.start, 'end': segment.end, 'text': segment.text}
+    results.append(segment_dict)
+
+subs = pysubs2.load_from_whisper(results)
+# save srt file
+subs.save(srt_file_name + '.srt')
+# save ass file
+# subs.save(file_name + '.ass')
